ccf9b5106546b84a1a68d69d28404d07304e6051
pubcomalger-crypto <pubcom.alger@gmail.com>
2025-12-30 11:42:55 +0100

    اميرات العرائس
    اميرات العرائس

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const profileConfig = {
     // Informations de base
 
 
-    name: "COCA",
-    bio: "SALAM",
-    profileImage: "https://i.ibb.co/NgKMhgMT/cocacola.jpg",
+    name: "أميرات الجمال والعرائس",
+    bio: "👑🧴🎁👛من عالمنا إلى أناقتك الملكية 👑👸🏻👰🏻💎🧴⌚ مواد تجميل طبيعية للبشرة 🧼🧴🧴زيوت طبيعية للشعر🥥🧄وإكسسوارات 💍💍للطلب التواصل معنا 0554997412",
+    profileImage: "https://scontent.falg6-2.fna.fbcdn.net/v/t39.30808-6/605634866_849126847896615_1481034572770496734_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=iQcuoZx8AjMQ7kNvwGSgxol&_nc_oc=Adk8RA90ZXbsCbMxC5IEHKHce5oM2OFVol_EN9ZKPdIA-eSooamjLZOiCrKwiHReAzE&_nc_zt=23&_nc_ht=scontent.falg6-2.fna&_nc_gid=1C-IpBGvE5r0t3kdLlC1lQ&oh=00_AfnxIhwlWhfsMuuARbotzYxHoUl5mnoOyVbY_j-fVg15Kw&oe=69597D41",
     
 
 
@@ -15,17 +15,17 @@ const profileConfig = {
 
     // Réseaux sociaux
     socialLinks: {
-        facebook: "https://www.facebook.com/share/16dysjBQvt/?mibextid=wwXIfr",
-        instagram: "https://www.instagram.com/riyadmahrez26.7?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-        tiktok: "https://www.tiktok.com/@chebkhaled?is_from_webapp=1&sender_device=pc",
-        whatsapp: "+213540253369",
+        facebook: "https://www.facebook.com/share/1GvjipXJjd/",
+        instagram: "https://www.instagram.com/queens_world_dz/?utm_source=qr&igsh=anJ0eTBtaHoxbjQ3#",
+        tiktok: "https://www.tiktok.com/@doctorcosmetique?_r=1&_t=ZS-92YgdofLgWu",
+        whatsapp: "+213554997412",
         telegram: "+213 562 33 14 08",
         linkedin: "daisyparkhouse",
         snapchat: "https://www.snapchat.com/@sheikh_tidiane?sender_web_id=792b2a4d-05d3-4e8f-a5e0-1aeb89eba722&device_type=desktop&is_copy_url=true",
         email: "pubcom.alger@gmail.com",
         website: "https://www.youtube.com/",
-        maps: "https://share.google/T1z7NAFbftnLYNKZy",
-        phone: "+213658546209"
+        maps: "https://maps.app.goo.gl/jDi5cgCvQj5uzQvH6",
+        phone: "+213 554 99 74 12"
     },
     
 
@@ -40,13 +40,13 @@ const profileConfig = {
         instagram: true,     // Afficher Instagram
         tiktok: true,        // Afficher TikTok
         whatsapp: true,      // Afficher WhatsApp
-        telegram: true,      // Afficher Telegram
+        telegram: false,      // Afficher Telegram
         linkedin: false,     // Afficher LinkedIn (mettre true pour l'activer)
         website: false,       // Afficher Site Web
         maps: true,          // Afficher Maps
         phone: true,         // Afficher Téléphone
-        snapchat: true,      // Afficher Snapchat
-        email: true,         // Afficher Email
+        snapchat: false,      // Afficher Snapchat
+        email: false,         // Afficher Email
     },
     
 };
@@ -83,3 +83,4 @@ if (typeof module !== 'undefined' && module.exports) {
 } else {
     window.profileConfig = profileConfig;
 }
+
